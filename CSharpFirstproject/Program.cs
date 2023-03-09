8a6e7b878810f1b2ec57e7a916413c1919d1f5ac
ancaroman1234 <anca.roman@endava.com>
2023-03-09 15:46:18 +0200

    test add

--- CSharpFirstproject/Program.cs
+++ CSharpFirstproject/Program.cs
@@ -1,4 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // comment
 Console.WriteLine("Hello, World!");
-Console.ReadLine();
+
+double BoxA, BoxB, BoxC;
+BoxA = 34;
+BoxB = 4;
+
+BoxC = BoxA + BoxB; // 38
+BoxC = BoxA - BoxB; //30
+BoxC = BoxA * BoxB; //136
+BoxC = BoxA % BoxB; //2 modulo division
+BoxC = BoxA / BoxB; // 8 bc we working with int . with double should be 8.5
+
+Console.WriteLine(BoxC);
