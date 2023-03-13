abfa2d97c5d9490ee1bdb23e3f2ec7fd9e7a6b65
ancaroman1234 <anca.roman@endava.com>
2023-03-13 13:05:53 +0200

    update

--- CSharpFirstproject/Program.cs
+++ CSharpFirstproject/Program.cs
@@ -11,5 +11,6 @@ BoxC = BoxA - BoxB; //30
 BoxC = BoxA * BoxB; //136
 BoxC = BoxA % BoxB; //2 modulo division
 BoxC = BoxA / BoxB; // 8 bc we working with int . with double should be 8.5
+BoxC= BoxA * BoxB;
 
 Console.WriteLine(BoxC);
